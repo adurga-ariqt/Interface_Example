efc8abc4de25a43cff63516e34afe2025cf1e9e8
adurga-ariqt <adurga@ariqt.com>
2022-11-08 19:39:42 +0530

    Add project files.

---
+++ Interface_Example/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface_Example
+{
+    interface IFirstInterface
+    {
+        void MyMthod();
+    }
+    interface IsecondInterface
+    {
+        void MyOtherMethod();
+    }
+    public  class Animal
+    {
+        public void MyMethod()
+        {
+            Console.WriteLine("some text");
+        }
+        public void MyOtherMethod()
+        {
+            Console.WriteLine("Some other text");
+        }
+    }
+}
---
+++ Interface_Example/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Interface_Example;
+
+Console.WriteLine("Hello, World!");
+Animal a = new Animal();
+a.MyMethod();
+a.MyOtherMethod();
